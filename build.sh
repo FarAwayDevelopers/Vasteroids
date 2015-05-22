49e881865d792c732c0ddb8edf4123149686c670
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2015-05-22 15:01:21 +0200

    Improved build-script

--- build.sh
+++ build.sh
@@ -1,5 +1,3 @@
-cd src
-
 valac \
     --pkg gee-0.8 \
     --pkg sdl2 \
@@ -11,5 +9,5 @@ valac \
     -X -lSDL2_ttf \
     -X -lSDL2_mixer \
     -X -lSDL2_gfx \
-    -o "../vasteroids" \
-    main.vala views.vala render_components.vala input_components.vala
+    -o "vasteroids" \
+    $(find src/ -name "*.vala" | xargs)
